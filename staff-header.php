8223d6b602694ac07f0c5496817981902559ef10
Sou-hardo <souhardo02@gmail.com>
2025-01-17 00:08:56 +0600

    Changed Homepage directory

--- staff-header.php
+++ staff-header.php
@@ -1,13 +1,13 @@
 <header>
     <nav>
         <div class="logo-block">
-            <img src="images/food-delivery.png" alt="logo" class="logo">
-            <h1><a href="homepage.php">Chewtopia</a></h1>
+            <img src="images/delicious.png" alt="logo" class="logo">
+            <h1><a href="staff-homepage.php">Chewtopia</a></h1>
         </div>
         <ul>
-            <li><a href="staff-add-item.php">ITEMS</a></li>
-            <li><a href="staff-add-restaurant.php">BRANCHES</a></li>
-            <li><a href="offer.php">OFFER</a></li>
+            <li><a href="staff-items.php">ITEMS</a></li>
+            <li><a href="staff-restaurants.php">BRANCHES</a></li>
+            <li><a href="staff-offers.php">OFFER</a></li>
             <li><a href="staff-add-driver.php">DRIVERS</a></li>
             <li><a href="staff-profile.php">PROFILE</a></li>
             <?php if (isset($_SESSION['user_id'])): ?>
