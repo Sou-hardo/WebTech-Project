8f57cd6fdc3ab12093af2d6f105dceb705be4770
Sou-hardo <souhardo02@gmail.com>
2025-01-17 21:46:34 +0600

    show offer image"

--- offers.php
+++ offers.php
@@ -12,6 +12,12 @@ require_once "dbconnect.php";
     <title>Offers</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fira%20Code">
+    <style>
+        .offer-detail-image {
+            max-width: 200px;
+            max-height: 300px;
+        }
+    </style>
 </head>
 
 <body>
@@ -27,12 +33,7 @@ require_once "dbconnect.php";
                     "SELECT voucher_id AS id, title, description, promo_code, percentage,
                                 start_date, expiry_date
                             FROM voucher
-                            UNION
-                            (SELECT discount_id AS id, title, description, NULL AS promo_code, percentage,
-                                start_date, expiry_date
-                            FROM discount)
-                            ORDER BY start_date DESC
-                            ";
+                            ORDER BY start_date DESC";
                 $result = mysqli_query($conn, $query);
                 $offers = mysqli_fetch_all($result, MYSQLI_ASSOC);
                 foreach ($offers as $row) {
@@ -44,9 +45,18 @@ require_once "dbconnect.php";
                     $start_date = $row["start_date"];
                     $expiry_date = $row["expiry_date"];
 
-                    echo "<div class='container-item'>
-                                <img src='images/offer-combo-food-based-on-the-season.jpg' alt='donut'>
-                                <div style='margin: 10px;' class='title'>$title</div>";
+                    echo "<div class='container-item'>";
+                    // Read and display image from JSON file
+                    $jsonFile = 'uploads/offers/' . $id . '.json';
+                    if (file_exists($jsonFile)) {
+                        $jsonData = json_decode(file_get_contents($jsonFile), true);
+                        $imageData = $jsonData['image_data'];
+                        $mimeType = $jsonData['mime_type'];
+                        echo "<img class='offer-detail-image' src='data:$mimeType;base64,$imageData' alt='$title'>";
+                    } else {
+                        echo "<img class='offer-detail-image' src='images/blank-image.jpg' alt='offer'>";
+                    }
+                    echo "<div style='margin: 10px;' class='title'>$title</div>";
                     echo "<div style='margin: 10px;'>Expiry Date: $expiry_date</div>";
                     echo (is_null($promo_code)) ?  "<br>" : "<div>PROMO_CODE: $promo_code</div>";
                     echo "<div style='margin: 10px;'>
